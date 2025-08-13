02fb0ccbc12d739fdc84bf75c0262dfc2e04dea0
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-08-13 15:23:33 -0300

    actualizando JavaScript

--- inscripciones v2/Inscripciones 1er/src/index.js
+++ inscripciones v2/Inscripciones 1er/src/index.js
@@ -3,20 +3,37 @@ const form2 = document.getElementById('paso2');
 const form3 = document.getElementById('paso3');
 
 const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn3');
+const btnAtras = document.getElementById('btn-prev');
+
+const pasoActual = [form1,form2,form3];
 
 const barra = document.querySelector('.relleno');
 
 btn1.addEventListener('click', () =>{
-    form1.style.display="none"
-    form2.style.display = '';
-    barra.style.width = '35%';
-    
-})
-btn2.addEventListener('click', () =>{
-    form2.style.display="none"
-    form3.style.display = '';
-    barra.style.width = '75%'; 
+    // validar datos
 
+    // -----------------------
+    for (let i = 0; i < pasoActual.length; i++) {
+        if(pasoActual[i]===form1){
+            form1.style.display="none"
+            form2.style.display = '';
+            barra.style.width = '35%';
+            return pasoActual[i]
+        }
+        else if(pasoActual[i]===form2){
+            form2.style.display="none"
+            form3.style.display = '';
+            barra.style.width = '75%';
+            return pasoActual[i]
+        }
+        else{
+            barra.style.width = '100%'
+        }
+    }
+})
+
+btnAtras.addEventListener('click',() =>{
+    form1.style.display = '';
+    form2.style.display = 'none';
+    barra.style.width = '0%';
 })
